485dba8db73f68c50aa9f6cfe7cef4e8cdf995d2
mandi7469 <mandi7469@aol.com>
2024-08-07 19:46:37 -0400

    completed userdata seed file

---
+++ seeds/userData.js
@@ -0,0 +1,26 @@
+[
+  {
+    username: "rachel_green",
+    password: "password123",
+  },
+  {
+    username: "phoebe_buffay",
+    password: "password123",
+  },
+  {
+    username: "chandler_bing",
+    password: "password123",
+  },
+  {
+    username: "ross_geller",
+    password: "password123",
+  },
+  {
+    username: "monica_geller",
+    password: "password123",
+  },
+  {
+    username: "joey_tribbiani",
+    password: "password123",
+  },
+];
